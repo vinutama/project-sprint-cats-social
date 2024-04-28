17bf2aa5e313baf954844fa5ca613d87fdb9a301
vinutama <muhammadkhevin@gmail.com>
2024-04-29 01:25:28 +0700

    add db conn pool, start fiber app

---
+++ database/db.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	cfg "cats-social/config"
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var (
+	dbName            = cfg.EnvConfigs.DbName
+	dbHost            = cfg.EnvConfigs.DbHost
+	dbPass            = cfg.EnvConfigs.DbPassword
+	dbUser            = cfg.EnvConfigs.DbUser
+	dbPort            = cfg.EnvConfigs.DbPort
+	dbTimeout         = 30 * time.Second
+	dbMaxConnLifeTime = 60 * time.Minute
+	dbMaxConnIdleTime = 5 * time.Minute
+	dbMaxConn         = int32(100)
+	dbMinConn         = int32(0)
+)
+
+func GetConnPool() *pgxpool.Pool {
+	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	cfg, err := pgxpool.ParseConfig(dbURL)
+	if err != nil {
+		log.Fatal("Error when parsing config DB URL: ", err)
+	}
+
+	cfg.MaxConnLifetime = dbMaxConnLifeTime
+	cfg.MaxConnIdleTime = dbMaxConnIdleTime
+	cfg.MaxConns = dbMaxConn
+	cfg.MinConns = dbMinConn
+	cfg.ConnConfig.ConnectTimeout = dbTimeout
+
+	dbPool, err := pgxpool.NewWithConfig(context.Background(), cfg)
+	if err != nil {
+		log.Fatal("Error when creating Database Pool Context: ", err)
+	}
+
+	return dbPool
+}
--- go.mod
+++ go.mod
@@ -12,7 +12,11 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/klauspost/compress v1.17.8 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -37,6 +41,7 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
