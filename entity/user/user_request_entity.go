1a69a703d078acbfb6b5b66b7a330dd2a58ffc63
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-04-29 13:54:01 +0700

    add user login request and response entity

--- entity/user/user_request_entity.go
+++ entity/user/user_request_entity.go
@@ -6,3 +6,8 @@ type UserRegisterRequest struct {
 	Email    string `json:"email" validate:"required"`
 	Password string `json:"password" validate:"required,min=5,max=15"`
 }
+
+type UserLoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=5,max=15"`
+}
