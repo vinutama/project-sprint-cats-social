7d76831624da36d0ade130de0fe56b194aa8dfa8
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-04-30 23:04:01 +0700

    add cat, request, response entity and custom validator for boolean and regex

--- entity/cat/cat_response_entity.go
+++ entity/cat/cat_response_entity.go
@@ -8,3 +8,20 @@ type CatCreateResponse struct {
 	Message string                 `json:"message"`
 	Data    *CatCreateDataResponse `json:"data"`
 }
+
+type CatSearchDataResponse struct {
+	Id          string   `json:"id"`
+	Name        string   `json:"name"`
+	Race        string   `json:"race"`
+	Sex         string   `json:"sex"`
+	AgeInMonth  int      `json:"ageInMonth"`
+	ImageURLs   []string `json:"imageUrls"`
+	Description string   `json:"description"`
+	HasMatched  bool     `json:"hasMatched"`
+	CreatedAt   string   `json:"createdAt"`
+}
+
+type CatSearchResponse struct {
+	Messagge string                   `json:"message"`
+	Data     *[]CatSearchDataResponse `json:"data"`
+}
