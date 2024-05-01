dbc043ee7303fa1aa3f3d9a1c2225618c403ed99
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-05-01 23:07:24 +0700

    restructure search cat query

--- repository/cat/cat_repository_impl.go
+++ repository/cat/cat_repository_impl.go
@@ -4,6 +4,8 @@ import (
 	cat_entity "cats-social/entity/cat"
 	"context"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/jackc/pgx/v5"
@@ -45,19 +47,67 @@ func (repository *CatRepositoryImpl) Create(ctx context.Context, tx pgx.Tx, cat 
 }
 
 func (repository *CatRepositoryImpl) Search(ctx context.Context, tx pgx.Tx, searchQuery cat_entity.CatSearch) ([]cat_entity.Cat, error) {
-	query := fmt.Sprintf(`SELECT * FROM cats
-    WHERE
-        ($1 = '' OR id = $1) AND
-        ($2 = '' OR race = $2) AND
-        ($3 = '' OR sex = $3) AND
-        ($4 = '' OR name ILIKE '%%' || $4 || '%%') AND
-        ($5 = '' OR has_matched = CAST($5 AS BOOL)) AND
-        (CASE WHEN $6 >= 0 THEN age_in_month %s $6 ELSE TRUE END) AND
-        (CASE WHEN $7 = 'true' THEN user_id = $8 WHEN $7 = 'false' THEN user_id != $8 ELSE TRUE END)
-		ORDER BY created_at DESC
-        LIMIT $9 OFFSET $10;`, searchQuery.AgeCondition)
+	query := `SELECT * FROM cats`
+	var whereClause []string
+	params := []interface{}{}
 
-	rows, _ := tx.Query(ctx, query, searchQuery.Id, searchQuery.Race, searchQuery.Sex, searchQuery.Name, searchQuery.HasMatched, searchQuery.AgeInMonth, searchQuery.Owned, searchQuery.UserId, searchQuery.Limit, searchQuery.Offset)
+	if searchQuery.Id != "" {
+		whereClause = append(whereClause, fmt.Sprintf("id = $%s", strconv.Itoa(len(params)+1)))
+		params = append(params, searchQuery.Id)
+	}
+
+	if searchQuery.Race != "" {
+		whereClause = append(whereClause, fmt.Sprintf("race = $%s", strconv.Itoa(len(params)+1)))
+		params = append(params, searchQuery.Race)
+	}
+
+	if searchQuery.Sex != "" {
+		whereClause = append(whereClause, fmt.Sprintf("sex = $%s", strconv.Itoa(len(params)+1)))
+		params = append(params, searchQuery.Sex)
+	}
+
+	if searchQuery.Name != "" {
+		whereClause = append(whereClause, fmt.Sprintf("name ILIKE '%%' || $%s || '%%'", strconv.Itoa(len(params)+1)))
+		params = append(params, searchQuery.Name)
+	}
+
+	if searchQuery.HasMatched != "" {
+		whereClause = append(whereClause, fmt.Sprintf("has_matched = $%s", strconv.Itoa(len(params)+1)))
+		hasMatched, err := strconv.ParseBool(searchQuery.HasMatched)
+		if err != nil {
+			return []cat_entity.Cat{}, err
+		}
+		params = append(params, hasMatched)
+	}
+
+	if searchQuery.AgeInMonth > 0 {
+		whereClause = append(whereClause, fmt.Sprintf("(CASE WHEN $%s >= 0 THEN age_in_month %s $%s ELSE TRUE END)", strconv.Itoa(len(params)+1), searchQuery.AgeCondition, strconv.Itoa(len(params)+1)))
+		params = append(params, searchQuery.AgeInMonth)
+	}
+
+	if searchQuery.Owned != "" {
+		whereClause = append(whereClause, fmt.Sprintf("(CASE WHEN $%s = 'true' THEN user_id = $%s WHEN $%s = 'false' THEN user_id != $%s ELSE TRUE END)", strconv.Itoa(len(params)+1), strconv.Itoa(len(params)+2), strconv.Itoa(len(params)+1), strconv.Itoa(len(params)+2)))
+		params = append(params, searchQuery.Owned)
+		params = append(params, searchQuery.UserId)
+	}
+
+	if len(whereClause) > 0 {
+		query += " WHERE " + strings.Join(whereClause, " AND ")
+	}
+
+	query += " ORDER BY created_at DESC"
+
+	if searchQuery.Limit > 0 {
+		query += fmt.Sprintf(" LIMIT $%s OFFSET $%s", strconv.Itoa(len(params)+1), strconv.Itoa(len(params)+2))
+		params = append(params, searchQuery.Limit)
+		params = append(params, searchQuery.Offset)
+	}
+
+	rows, err := tx.Query(ctx, query, params...)
+	if err != nil {
+		return []cat_entity.Cat{}, err
+	}
+
 	cats, err := pgx.CollectRows(rows, pgx.RowToStructByName[cat_entity.Cat])
 	if err != nil {
 		return []cat_entity.Cat{}, err
